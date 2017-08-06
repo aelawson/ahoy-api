611a64cff2193a638ef4f366232786c8fb8c8334
Andrew Lawson <alawson@jwplayer.com>
2017-08-06 15:54:18 -0400

    Fix accidental tupleization (trailing comma)

--- src/services/response.py
+++ src/services/response.py
@@ -10,7 +10,7 @@ class Response:
             self.headers = Headers.get_default_headers(body)
         else:
             self.headers = headers
-        self.body = body,
+        self.body = body
         self.status = StatusCodes.get_status_message(status_code)
 
     def set_status(self, status_code):
