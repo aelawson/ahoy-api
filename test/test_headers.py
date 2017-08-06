1df6140daa80ae15566ceba5ef841bf387efa9b6
Andrew Lawson <alawson@jwplayer.com>
2017-08-06 14:39:29 -0400

    Add header tests.

---
+++ test/test_headers.py
@@ -0,0 +1,12 @@
+from app.services.headers import Headers
+
+class TestHeaders:
+
+    def test_default_headers(self):
+        data = b'abcdefg'
+        expected_result = [
+            ('Content-Type', 'text/plain'),
+            ('Content-Length', str(len(data)))
+        ]
+        actual_result = Headers.get_default_headers(data)
+        assert actual_result == expected_result
