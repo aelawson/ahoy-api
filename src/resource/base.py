86fa199e3beaf25bcbc2fc16babe67987a17a6da
Andrew Lawson <alawson@jwplayer.com>
2017-08-07 00:57:11 -0400

    Pass back values from resource properly.

--- src/resource/base.py
+++ src/resource/base.py
@@ -23,20 +23,20 @@ class BaseResource:
 
     @handle_response
     def get_handler(self, *args, **kwargs):
-        self.get(*args, **kwargs)
+        return self.get(*args, **kwargs)
 
     @handle_response
     def post_handler(self, *args, **kwargs):
-        self.post(*args, **kwargs)
+        return self.post(*args, **kwargs)
 
     @handle_response
     def put_handler(self, *args, **kwargs):
-        self.put(*args, **kwargs)
+        return self.put(*args, **kwargs)
 
     @handle_response
     def patch_handler(self, *args, **kwargs):
-        self.patch(*args, **kwargs)
+        return self.patch(*args, **kwargs)
 
     @handle_response
     def delete_handler(self, *args, **kwargs):
-        self.delete(*args, **kwargs)
+        return self.delete(*args, **kwargs)
--- src/utilities/decorators.py
+++ src/utilities/decorators.py
@@ -1,5 +1,5 @@
 
 def handle_response(fn):
     def __wrapper(*args, **kwargs):
-        fn(*args, **kwargs)
+        return fn(*args, **kwargs)
     return __wrapper
