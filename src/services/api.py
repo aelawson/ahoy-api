7a19aedc9e438b4fc33fe3e98cc1de7f8c5f4be8
Andrew Lawson <alawson@jwplayer.com>
2017-08-07 00:57:44 -0400

    Add basic routing logic.

--- src/services/api.py
+++ src/services/api.py
@@ -1,11 +1,36 @@
+from src.services.response import Response
 
 class Api:
 
     def __init__(self):
         self.routes = {}
 
+    def handle_request(self, start_response, env):
+        method = env.get('REQUEST_METHOD')
+        query = env.get('QUERY_STRING')
+        path = env.get('RAW_URI')
+        params = None
+
+        resource = self.routes.get(path)
+        if resource:
+            method_handler = getattr(
+                resource,
+                '{method}_handler'.format(method=method.lower())
+            )
+            response_data = method_handler(query, params)
+            return Response.response(
+                start_response,
+                status_code=response_data.get('status_code'),
+                headers=response_data.get('headers'),
+                body=response_data.get('body')
+            )
+        else:
+            return Response.response(start_response, 404)
+
     def route(self, route):
-        def __decorator(fn):
-            self.routes[route] = fn
-            return fn
+        def __decorator(cls):
+            self.routes[route] = cls()
+            return cls
         return __decorator
+
+api = Api()
