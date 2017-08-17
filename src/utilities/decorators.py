e55d70afc404c59f7f02ef1610789b003dfc0e5e
Andrew Lawson <alawson@jwplayer.com>
2017-08-16 22:30:51 -0400

    Add header decorator. Change internal name of wrapper to decorator.

--- src/utilities/decorators.py
+++ src/utilities/decorators.py
@@ -1,8 +1,15 @@
 
 def handle_response(fn):
-    def __wrapper(*args, **kwargs):
+    def __decorator(*args, **kwargs):
         try:
             return fn(*args, **kwargs)
         except NotImplementedError:
             return Response.response(405)
-    return __wrapper
+    return __decorator
+
+def header(self, header, value):
+    def __decorator(fn):
+        response = fn(*args, **kwargs)
+        response['headers'][header] = value
+        return response
+    return __decorator
