d21a12e53293e90be87ce4ae84e2edca41d6ec53
Andrew Lawson <alawson@jwplayer.com>
2017-08-19 17:59:48 -0400

    Add DB_HOST env var to docker-compose. Add new commands to run.py docker entrypoint for migration and rollback.

--- run.py
+++ run.py
@@ -9,6 +9,10 @@ args = parser.parse_args()
 
 if args.cmd.lower() == "test":
     command = "pytest" + ' '.join(args.args)
+elif args.cmd.lower() == 'migrate':
+    command = 'orator migrate -c ./src/services/db_config.py'
+elif args.cmd.lower() == 'rollback':
+    command = 'orator migrate:reset  -c ./src/services/db_config.py'
 elif args.cmd.lower() == "run":
     command = "gunicorn -b :8000 --reload app:application --enable-stdio-inheritance"
 
