ca4136d787415bcbfde6ea2ad14fff9399c5e300
Andrew Lawson <alawson@jwplayer.com>
2017-08-19 17:59:21 -0400

    Fix usage of undefined identifier instead of string.

--- migrations/2017_08_19_212014_create_table_stages.py
+++ migrations/2017_08_19_212014_create_table_stages.py
@@ -9,7 +9,7 @@ class CreateTableStages(Migration):
         """
         with self.schema.create('stages') as table:
             table.increments('id')
-            table.small_integer(stage_id).unsigned()
+            table.small_integer('stage_id').unsigned()
             table.string('stage_name', 50)
             table.small_integer('stage_order').unsigned()
 
