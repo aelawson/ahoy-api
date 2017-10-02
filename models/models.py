ea38ffc16177e3594efe4a04af87d461c0829839
Andrew Lawson <alawson@jwplayer.com>
2017-10-02 17:05:33 -0400

    Update models to use proper relations. Fix foreign keys to use actual field names.

--- models/models.py
+++ models/models.py
@@ -1,30 +1,34 @@
 from orator import Model
-from orator.orm import has_one, has_many, belongs_to
+from orator.orm import has_one, has_many, belongs_to, belongs_to_many
 
 class Team(Model):
 
-    @has_one('foreign_key')
+    @has_one('id')
     def plan(self):
         return Plan
 
 class Plan(Model):
 
-    @has_many('foreign_key')
+    @has_many('id')
     def releases(self):
         return Release
 
-    @belongs_to('foreign_key')
+    @belongs_to('plan_id')
     def team(self):
         return Team
 
 class Release(Model):
 
-    @belongs_to('foreign_key')
+    @belongs_to('release_id')
     def plan(self):
         return Plan
 
+    @belongs_to('id')
+    def stage(self):
+        return Stage
+
 class Stage(Model):
 
-    @belongs_to('foreign_key')
+    @has_many('release_id')
     def release(self):
         return Release
