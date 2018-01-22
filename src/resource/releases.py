4f9d773f925aa1412ae825f33884eab8dac12e0f
Andrew Lawson <alawson@jwplayer.com>
2018-01-21 21:28:15 -0800

    Add cut action resource.

--- src/resource/releases.py
+++ src/resource/releases.py
@@ -1,8 +1,9 @@
-from models.models import Plan, Release
+from models.models import Plan, Release, Stage
 
 from src.resource.base import BaseResource
 from src.services.api import api
 from src.services.db import db
+from src.services.release import ReleaseService
 
 @api.route('/teams/plans/<plan_id>/releases/')
 @api.methods(['HEAD', 'OPTIONS', 'GET'])
@@ -28,4 +29,30 @@ class ReleaseResource(BaseResource):
         return {
             'status_code': 200,
             'body': release.serialize()
+        }
+
+@api.route('/teams/plans/releases/<release_id>/cut/')
+@api.methods(['HEAD', 'OPTIONS', 'POST'])
+class ReleaseActionResource(BaseResource):
+
+    @api.json
+    def post(*args, **kwargs):
+        release_id = kwargs.get('release_id')
+        major = kwargs.get('major')
+        minor = kwargs.get('minor')
+        patch = kwargs.get('patch')
+
+        release = Release.find(release_id)
+
+        try:
+            ReleaseService.cut(release, major, minor, patch)
+        except Exception as e:
+            raise e
+
+        next_stage = Stage.where('name', '=', 'Staging').first()
+        release.stage_id = next_stage.id
+        release.save()
+
+        return {
+            'status_code': 200
         }
