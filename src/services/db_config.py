d380cd1ab7a1d96cd6465cca32f0a6a3e71c2aae
Andrew Lawson <alawson@jwplayer.com>
2018-02-27 00:55:13 -0500

    Update port for database.

--- src/services/db_config.py
+++ src/services/db_config.py
@@ -4,6 +4,7 @@ DATABASES = {
     'postgres': {
         'driver': 'postgres',
         'host': os.environ.get('DB_HOST', 'localhost'),
+        'port': os.environ.get('DB_PORT', '5432'),
         'database': os.environ.get('DB_DATABASE', 'database'),
         'user': os.environ.get('DB_USER', 'root'),
         'password': os.environ.get('DB_PASS', 'root'),
