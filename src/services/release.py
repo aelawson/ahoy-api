b3f2a30361768c8e89f6ce171b35d0865f2a4528
Andrew Lawson <alawson@jwplayer.com>
2018-02-27 00:59:43 -0500

    Add release action. Update tag logic. Add repo config.

--- src/services/release.py
+++ src/services/release.py
@@ -2,6 +2,8 @@ import random, string
 
 from git import Repo
 
+from src.services.config import Config
+
 class ReleaseService:
 
     @classmethod
@@ -17,12 +19,40 @@ class ReleaseService:
             '/tmp/{dir}'.format(dir=write_dir),
             branch='develop'
         )
+
+        with repo.config_writer() as cw:
+            cw.set_value('user', 'email', Config['git']['email'])
+            cw.set_value('user', 'name', Config['git']['author'])
+
         git = repo.git
 
-        sort_key = lambda t: t.path.split('/')[-1]
-        tags = iter(sorted(repo.tags, key=sort_key, reverse=True))
+        next_tag = cls.__get_next_tag(repo.tags, major, minor, patch)
 
-        next_tag = cls.__get_next_tag(tags, major, minor, patch)
+        git.checkout('develop')
+        git.merge('origin/master')
+        git.tag(next_tag)
+
+    @classmethod
+    def release(cls, release):
+        alphabet = (string.ascii_uppercase +
+            string.ascii_lowercase + string.digits)
+        write_dir = ''.join(
+            random.choice(alphabet) for _ in range(16)
+        )
+
+        repo = Repo.clone_from(
+            release.repo_url,
+            '/tmp/{dir}'.format(dir=write_dir),
+            branch='develop'
+        )
+
+        with repo.config_writer() as cw:
+            cw.set_value('user', 'email', Config['git']['email'])
+            cw.set_value('user', 'name', Config['git']['author'])
+
+        git = repo.git
+
+        last_tag = cls.__get_last_tag(repo.tags)
 
         git.checkout('master')
         git.merge('develop')
@@ -31,11 +61,7 @@ class ReleaseService:
     @classmethod
     def __get_next_tag(cls, tags, major, minor, patch):
 
-        try:
-            last_tag = next(tags).path.split('/')[-1]
-        except Exception:
-            last_tag = '0.0.0'
-
+        last_tag = cls.__get_last_tag(tags)
         last_major, last_minor, last_patch = last_tag.split('.')
 
         if not minor and not patch:
@@ -54,3 +80,13 @@ class ReleaseService:
         tag_strings = map(lambda t: str(t), [major, minor, patch])
 
         return '.'.join(tag_strings)
+
+    @classmethod
+    def __get_last_tag(cls, tags):
+        sort_key = lambda t: t.path.split('/')[-1]
+        sorted_tags = iter(sorted(tags, key=sort_key, reverse=True))
+
+        try:
+            return next(sorted_tags).path.split('/')[-1]
+        except Exception:
+            return '0.0.0'
